40a3054f02245cf68b5f3818498145ffa6afaa29
Vuynee <179316784+Vuyne@users.noreply.github.com>
2025-09-10 11:23:41 -0400

    i will change a lot of stuff

---
+++ Work/My project (1)/Assets/Scripts/WallRun.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class WallRun : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float wallMaxDistance = 1;
+    public float wallspeedMultiplier = 1.2f;
+    public float minimumHeight = 1.2f;
+    public float maxAngleRoll = 20;
+
+    public float jumpDuration = 1;
+    public float wallBouncing = 3;
+    public float cameraTransitionDuration = 1;
+
+    public float wallGravityDownForce = 20f;
+
+    public bool useSprint ;
+
+   
+
+    
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
