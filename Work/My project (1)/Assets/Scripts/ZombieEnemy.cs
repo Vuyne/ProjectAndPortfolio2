1b13de4438a8f8ce0b6f0001fe44817b03c06036
Valeria <valemalegre.me@gmail.com>
2025-09-23 17:38:50 -0400

    checkpoint popup
    done prefab

--- Work/My project (1)/Assets/Scripts/ZombieEnemy.cs
+++ Work/My project (1)/Assets/Scripts/ZombieEnemy.cs
@@ -5,12 +5,16 @@ using UnityEngine.AI; //for navMesh
 public class ZombieEnemy : EnemyBase
 {
     [SerializeField] NavMeshAgent agent; //to walk 
+    [SerializeField] Animator anim;
     [SerializeField] Rigidbody rb;
 
 
     // Update is called once per frame
     void Update()
     {
+
+        setAnimLocomotion();
+
         if (!playerInTrigger || gameManager.instance == null || gameManager.instance.player == null)
             return;
 
@@ -20,5 +24,14 @@ public class ZombieEnemy : EnemyBase
         }
     }
 
+    void setAnimLocomotion()
+    {
+        //float agentSpeedCurr = agent.velocity.normalized.magnitude;
+        //float animSpeedCurr = anim.GetFloat("Speed");
+
+        // anim.SetFloat("Speed", Mathf.Lerp(animSpeedCurr, agentSpeedCurr, Time.deltaTime * animTransSpeed));
+
+        anim.SetFloat("Speed", agent.velocity.normalized.magnitude);
+    }
     
 }
