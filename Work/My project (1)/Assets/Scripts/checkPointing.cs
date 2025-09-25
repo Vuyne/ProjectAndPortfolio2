0c6fab096823ec31256056c5c6430db6880c9015
Valeria <valemalegre.me@gmail.com>
2025-09-24 23:03:16 -0400

    checkpoint and teleporting working
    :)

---
+++ Work/My project (1)/Assets/Scripts/checkPointing.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.VFX;
+
+public class checkPointing : MonoBehaviour
+{
+    [SerializeField] Renderer model;
+
+    Color colorOrig;
+
+    private void Start()
+    {
+        colorOrig = model.material.color;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && gameManager.instance.playerSpawnPos.transform.position != transform.position)
+        {
+            gameManager.instance.playerSpawnPos.transform.position = transform.position; //move spawn pos to this checkpoint
+            StartCoroutine(checkpointFeedback());
+        }
+    }
+
+    IEnumerator checkpointFeedback()
+    {
+        gameManager.instance.checkpointPopup.SetActive(true); //turn on
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(1.25f); //how long we wait
+        model.material.color = colorOrig;
+        gameManager.instance.checkpointPopup.SetActive(false); //turn it off
+    }
+}
